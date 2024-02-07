d71d02d208483f4552cee9fa5fed213ccd58ade7
Imdvlpr99 <imdvlpr.agency@gmail.com>
2024-02-07 20:47:59 +0700

    feat : add constants for colors

---
+++ lib/utils/color.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+const Color black = Color(0xFF000E08);
+const Color darkBlue = Color(0xFF3D4A7A);
+const Color white = Color(0xFFFFFFFF);
+const Color ashGrey = Color(0xFF797C7B);
+const Color gray = Color(0xFFDEEBFF);
