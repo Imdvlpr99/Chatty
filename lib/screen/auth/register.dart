f193741be233150e16e2531b210d5aa1fa8ff5f2
Imdvlpr99 <imdvlpr.agency@gmail.com>
2024-02-09 23:27:51 +0700

    feat : create register page

---
+++ lib/screen/auth/register.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  const Register({super.key});
+
+  @override
+  State<StatefulWidget> createState() => RegisterState();
+}
+
+class RegisterState extends State<Register> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+
+        ),
+      ),
+    );
+  }
+
+}
