671653531ee58d849e4dce8330a2fffb6918f112
Imdvlpr99 <imdvlpr.agency@gmail.com>
2024-02-09 23:24:47 +0700

    feat : add new color

--- lib/utils/color.dart
+++ lib/utils/color.dart
@@ -1,7 +1,9 @@
 import 'dart:ui';
 
 const Color black = Color(0xFF000E08);
+const Color blackSoft = Color(0xFF0F172A);
 const Color darkBlue = Color(0xFF3D4A7A);
 const Color white = Color(0xFFFFFFFF);
 const Color ashGrey = Color(0xFF797C7B);
 const Color gray = Color(0xFFDEEBFF);
+const Color softGray = Color(0xFFE2E8F0);
