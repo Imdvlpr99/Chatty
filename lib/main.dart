3fdd1a1cf22fb3309bf6c689e0cfb1f9a5642fc2
Imdvlpr99 <imdvlpr.agency@gmail.com>
2024-02-10 18:59:39 +0700

    feat : initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:chatty/screen/splash.dart';
 import 'package:chatty/utils/color.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'utils/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
