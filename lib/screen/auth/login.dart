e912d3fb2521cf826f86cb4ac60cf5d0ba1a801d
Imdvlpr99 <imdvlpr.agency@gmail.com>
2024-02-14 22:29:44 +0700

    fix : change widget from scaffold to container

--- lib/screen/auth/login.dart
+++ lib/screen/auth/login.dart
@@ -34,170 +34,164 @@ class LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: white,
-      body: Container(
+    return Container(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
         color: white,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: SingleChildScrollView(
-            child: Column(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            CustomTextField(
+              controller: _emailController,
+              labelText: emailHint,
+              textInputType: TextInputType.emailAddress,
+              prefix: Iconsax.sms,
+            ),
+            const SizedBox(height: 16),
+            CustomTextField(
+              controller: _passwordController,
+              labelText: passwordHint,
+              textInputType: TextInputType.visiblePassword,
+              isObscureText: true,
+              prefix: Iconsax.lock,
+            ),
+            const SizedBox(height: 16),
+            Row(
               children: [
-                CustomTextField(
-                  controller: _emailController,
-                  labelText: emailHint,
-                  textInputType: TextInputType.emailAddress,
-                  prefix: Iconsax.sms,
+                CustomCheckBox(
+                  title: rememberMe,
+                  isChecked: _isRemembered,
+                  onValueChanged: (bool value) {
+                    setState(() {
+                      _isRemembered = value;
+                    });
+                  },
                 ),
-                const SizedBox(height: 16),
-                CustomTextField(
-                  controller: _passwordController,
-                  labelText: passwordHint,
-                  textInputType: TextInputType.visiblePassword,
-                  isObscureText: true,
-                  prefix: Iconsax.lock,
-                ),
-                const SizedBox(height: 16),
-                Row(
-                  children: [
-                    CustomCheckBox(
-                      title: rememberMe,
-                      isChecked: _isRemembered,
-                      onValueChanged: (bool value) {
-                        setState(() {
-                          _isRemembered = value;
-                        });
-                      },
-                    ),
-                    const Spacer(),
-                    InkWell(
-                      onTap: () {
+                const Spacer(),
+                InkWell(
+                  onTap: () {
 
-                      },
-                      child: Text(
-                        forgotPassword,
-                        style: semiBold12spDarkBlue,
-                      ),
-                    )
-                  ],
-                ),
-                const SizedBox(height: 24),
-                SizedBox(
-                  width: double.infinity,
-                  height: 56,
-                  child: ElevatedButton(
-                      style: buttonPrimary,
-                      onPressed: () {
-                        if (_isRemembered) {
-                          SharedPreferencesUtils.setString(userName, _emailController.text);
-                        } else {
-                          SharedPreferencesUtils.setString(userName, '');
-                        }
-                      },
-                      child: Text(
-                        login,
-                        style: bold16spWhite,
-                      )
+                  },
+                  child: Text(
+                    forgotPassword,
+                    style: semiBold12spDarkBlue,
                   ),
-                ),
-                const SizedBox(height: 24),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 32),
-                  height: 32,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                          child: Container(
-                            height: 2,
-                            decoration: BoxDecoration(
-                                color: grey,
-                                borderRadius: BorderRadius.circular(20)
-                            ),
-                          )
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8),
-                        child: Text(
-                          loginOtherOption,
-                          style: semiBold12spAshGrey,
-                        ),
-                      ),
-                      Expanded(
-                          child: Container(
-                            height: 2,
-                            decoration: BoxDecoration(
-                                color: grey,
-                                borderRadius: BorderRadius.circular(50)
-                            ),
-                          )
-                      )
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 24),
-                Row(
-                  children: [
-                    Expanded(
-                      child: SizedBox(
-                        height: 56,
-                        child: ElevatedButton(
-                          style: buttonSecondary,
-                          onPressed: () {},
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              SvgPicture.asset(
-                                'assets/images/google_icon.svg',
-                                width: 20,
-                                height: 20,
-                              ),
-                              const SizedBox(width: 8),
-                              Text(
-                                google,
-                                style: medium14spAshGrey,
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 8),
-                    Expanded(
-                      child: SizedBox(
-                        height: 56,
-                        child: ElevatedButton(
-                          style: buttonSecondary,
-                          onPressed: () {},
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              SvgPicture.asset(
-                                'assets/images/apple_icon.svg',
-                                width: 20,
-                                height: 20,
-                                colorFilter: const ColorFilter.mode(ashGrey, BlendMode.srcIn),
-                              ),
-                              const SizedBox(width: 8),
-                              Text(
-                                apple,
-                                style: medium14spAshGrey,
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                const SizedBox(height: 20)
+                )
               ],
             ),
-          ),
-        ),
-      ),
+            const SizedBox(height: 24),
+            SizedBox(
+              width: double.infinity,
+              height: 56,
+              child: ElevatedButton(
+                  style: buttonPrimary,
+                  onPressed: () {
+                    if (_isRemembered) {
+                      SharedPreferencesUtils.setString(userName, _emailController.text);
+                    } else {
+                      SharedPreferencesUtils.setString(userName, '');
+                    }
+                  },
+                  child: Text(
+                    login,
+                    style: bold16spWhite,
+                  )
+              ),
+            ),
+            const SizedBox(height: 24),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              height: 32,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                      child: Container(
+                        height: 2,
+                        decoration: BoxDecoration(
+                            color: grey,
+                            borderRadius: BorderRadius.circular(20)
+                        ),
+                      )
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: Text(
+                      loginOtherOption,
+                      style: semiBold12spAshGrey,
+                    ),
+                  ),
+                  Expanded(
+                      child: Container(
+                        height: 2,
+                        decoration: BoxDecoration(
+                            color: grey,
+                            borderRadius: BorderRadius.circular(50)
+                        ),
+                      )
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 24),
+            Row(
+              children: [
+                Expanded(
+                  child: SizedBox(
+                    height: 56,
+                    child: ElevatedButton(
+                      style: buttonSecondary,
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SvgPicture.asset(
+                            'assets/images/google_icon.svg',
+                            width: 20,
+                            height: 20,
+                          ),
+                          const SizedBox(width: 8),
+                          Text(
+                            google,
+                            style: medium14spAshGrey,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: SizedBox(
+                    height: 56,
+                    child: ElevatedButton(
+                      style: buttonSecondary,
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SvgPicture.asset(
+                            'assets/images/apple_icon.svg',
+                            width: 20,
+                            height: 20,
+                            colorFilter: const ColorFilter.mode(ashGrey, BlendMode.srcIn),
+                          ),
+                          const SizedBox(width: 8),
+                          Text(
+                            apple,
+                            style: medium14spAshGrey,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(height: 20)
+          ],
+        )
     );
   }
 
