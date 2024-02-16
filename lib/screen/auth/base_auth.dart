d926ea1b60e2401129830fe51380579ed736808d
Imdvlpr99 <imdvlpr.agency@gmail.com>
2024-02-16 12:33:13 +0700

    fix : refactor code

--- lib/screen/auth/base_auth.dart
+++ lib/screen/auth/base_auth.dart
@@ -45,101 +45,96 @@ class BaseAuthState extends State<BaseAuth> with SingleTickerProviderStateMixin 
     SystemChrome.setSystemUIOverlayStyle(darkMode);
     return Scaffold(
         backgroundColor: blackSoft,
-        body: SafeArea(
-          child: Stack(
-            children: [
-              Positioned(
-                  top: 0,
-                  right: 0,
-                  left: 0,
-                  child: SvgPicture.asset(
-                    'assets/images/header_bg.svg',
-                    fit: BoxFit.cover,
-                    alignment: Alignment.topCenter,
-                  )
-              ),
-              Positioned.fill(
-                child: SingleChildScrollView(
-                  child: Container(
-                    margin: const EdgeInsets.only(top: 150),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 24),
-                          child: Text(
-                            _selectedIndex == 0 ? loginTitle : registerTitle,
-                            style: bold24spWhite,
-                          ),
+        body: Stack(
+          children: [
+            Positioned(
+                top: 0,
+                right: 0,
+                left: 0,
+                child: SvgPicture.asset(
+                  'assets/images/header_bg.svg',
+                  fit: BoxFit.cover,
+                  alignment: Alignment.topCenter,
+                )
+            ),
+            Positioned.fill(
+              child: SingleChildScrollView(
+                child: Container(
+                  margin: const EdgeInsets.only(top: 150),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 24),
+                        child: Text(
+                          _selectedIndex == 0 ? loginTitle : registerTitle,
+                          style: bold24spWhite,
                         ),
-                        const SizedBox(height: 8),
-                        Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 24),
-                          child: Text(
-                            authDesc,
-                            style: medium12spSmokeGrey,
-                          ),
+                      ),
+                      const SizedBox(height: 8),
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 24),
+                        child: Text(
+                          authDesc,
+                          style: medium12spSmokeGrey,
                         ),
-                        const SizedBox(height: 32),
-                        Container(
-                          decoration: const BoxDecoration(
-                            color: white,
-                            borderRadius: BorderRadius.vertical(top: Radius.circular(40)),
-                          ),
-                          child: Column(
-                            children: [
-                              Container(
-                                margin: const EdgeInsets.symmetric(vertical: 32, horizontal: 24),
-                                height: 52,
-                                decoration: BoxDecoration(
-                                  color: softGrey,
-                                  borderRadius: BorderRadius.circular(32),
-                                  boxShadow: [mainShadow],
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(5),
-                                  child: TabBar(
-                                    dividerColor: Colors.transparent,
-                                    controller: _tabController,
-                                    tabs: _tabs,
-                                    labelStyle: semiBold14spDarkBlue,
-                                    unselectedLabelStyle: medium14spAshGrey,
-                                    indicatorSize: TabBarIndicatorSize.tab,
-                                    indicator: BoxDecoration(
-                                      color: white,
-                                      borderRadius: BorderRadius.circular(48),
-                                      boxShadow: [mainShadow],
-                                    ),
-                                    onTap: (index) {
-                                      setState(() {
-                                        _selectedIndex = index;
-                                      });
-                                    },
+                      ),
+                      const SizedBox(height: 32),
+                      Container(
+                        decoration: const BoxDecoration(
+                          color: white,
+                          borderRadius: BorderRadius.vertical(top: Radius.circular(40)),
+                        ),
+                        child: Column(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.symmetric(vertical: 32, horizontal: 24),
+                              height: 52,
+                              decoration: BoxDecoration(
+                                color: softGrey,
+                                borderRadius: BorderRadius.circular(32),
+                                boxShadow: [mainShadow],
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(5),
+                                child: TabBar(
+                                  dividerColor: Colors.transparent,
+                                  controller: _tabController,
+                                  tabs: _tabs,
+                                  labelStyle: semiBold14spDarkBlue,
+                                  unselectedLabelStyle: medium14spAshGrey,
+                                  indicatorSize: TabBarIndicatorSize.tab,
+                                  indicator: BoxDecoration(
+                                    color: white,
+                                    borderRadius: BorderRadius.circular(48),
+                                    boxShadow: [mainShadow],
                                   ),
+                                  onTap: (index) {
+                                    setState(() {
+                                      _selectedIndex = index;
+                                    });
+                                  },
                                 ),
                               ),
-                              Container(
-                                constraints: const BoxConstraints(
-                                    maxHeight: 800
-                                ),
-                                child: Container(
-                                  margin: const EdgeInsets.symmetric(vertical: 8),
-                                  child: TabBarView(
-                                    controller: _tabController,
-                                    children: _tabScreen,
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
+                            ),
+                            Container(
+                              margin: const EdgeInsets.symmetric(vertical: 8),
+                              height: MediaQuery.of(context).size.height,
+                              child: TabBarView(
+                                controller: _tabController,
+                                children: _tabScreen,
+                              ),
+                            )
+                          ],
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
-              )
-            ],
-          )
+              ),
+            )
+          ],
         )
     );
   }
